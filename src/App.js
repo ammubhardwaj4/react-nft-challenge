6866fde51ca0cfb1e23f1d5e32c320fb4998a051
ammubhardwaj4 <ammubhardwaj4@gmail.com>
2022-01-12 10:09:46 +0530

    cryptopunk file of clone added..

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import Header from "./components/Header";
+import "./App.css";
+import PunkList from "./components/PunkList";
+import Main from "./components/Main";
+
+function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xcA1a59d62457C193e5aC1350671F358CF4352711&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+
+    return getMyNfts();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <PunkList
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
